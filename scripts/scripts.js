4e16eb71eb47dfbd9bdf3701cddce640113e49d8
ashleyjoseph021 <ashleyjoseph01@proton.me>
2025-04-24 23:08:28 -0600

    html, css, js updated

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,56 @@
 console.log("Initializing js");
 
-function remeberTheme() {
-    toggleTheme(localStorage.getItem("isDarkTheme") == true)
+function rememberTheme() {
+    let theme = localStorage.getItem("isDarkTheme");
+    if (theme != null) {
+        setTheme(theme == "true")
+    } else {
+        if (window.matchMedia("prefers-color-scheme: dark")) {
+            setTheme(true)
+        } else {
+            setTheme(false)
+        }
+    }
 }
 
-remeberTheme();
+rememberTheme();
 
-function toggleTheme(isDarkTheme) {
+function toggleTheme() {
+    let theme = localStorage.getItem("isDarkTheme");
+    if (theme != null) {
+        if (theme == "true") {
+            setTheme(false);
+        } else {
+            setTheme(true);
+        }
+    } else {
+
+        let currentTheme = document.getElementById("css-theme").getAttribute("href");
+    
+        if (currentTheme == "../styles/light-theme.css") {
+            setTheme(true)
+        } else {
+            setTheme(false)
+        }
+    
+    }
+
+}
+
+function setTheme(isDarkTheme) {
+
+    let themeToggle = document.getElementById("theme-toggle");
+    let cssTheme = document.getElementById("css-theme");
 
     if (!isDarkTheme) {
-        document.getElementById("css-theme").setAttribute("href", "../styles/light-theme.css");
+        themeToggle.firstChild.classList.remove("fa-moon")
+        themeToggle.firstChild.classList.add("fa-sun")
+        cssTheme.setAttribute("href", "../styles/light-theme.css");
         localStorage.setItem("isDarkTheme", false);
     } else {
-        document.getElementById("css-theme").setAttribute("href", "../styles/dark-theme.css");
+        themeToggle.firstChild.classList.remove("fa-sun")
+        themeToggle.firstChild.classList.add("fa-moon")
+        cssTheme.setAttribute("href", "../styles/dark-theme.css");
         localStorage.setItem("isDarkTheme", true);
     }
 }
